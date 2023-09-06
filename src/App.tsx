68ffab3f53f2652781fdfa95cfb116593dd52233
Dikshita Patel <dikshipatel1999@gmail.com>
2023-09-05 23:29:03 -0400

    ramp fe challenge dikshita

---
+++ src/App.tsx
@@ -0,0 +1,116 @@
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react"
+import { InputSelect } from "./components/InputSelect"
+import { Instructions } from "./components/Instructions"
+import { Transactions } from "./components/Transactions"
+import { useEmployees } from "./hooks/useEmployees"
+import { usePaginatedTransactions } from "./hooks/usePaginatedTransactions"
+import { useTransactionsByEmployee } from "./hooks/useTransactionsByEmployee"
+import { EMPTY_EMPLOYEE } from "./utils/constants"
+import {Employee, Transaction} from "./utils/types"
+
+export function App() {
+  const { data: employees, ...employeeUtils } = useEmployees()
+  const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
+  const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
+  const [isLoading, setIsLoading] = useState(false)
+  const [allTransactions, setAllTransactions] = useState<Transaction[] | null>(null)
+
+  const transactions = useMemo(
+    () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
+    [paginatedTransactions, transactionsByEmployee]
+  )
+
+    useEffect(() => {
+        if (transactions !== null) {
+            setAllTransactions((prevTransactions) => {
+                if (prevTransactions === null) {
+                    return transactions
+                } else {
+                    return [...prevTransactions, ...transactions]
+                }
+            })
+        }
+    }, [transactions])
+
+    // Bug 4 fix
+
+    const loadAllTransactions = useCallback(async () => {
+        setIsLoading(true)
+        //transactionsByEmployeeUtils.invalidateData()
+        await employeeUtils.fetchAll()
+        setIsLoading(false)
+
+        // Buf Fix 5
+        await paginatedTransactionsUtils.fetchAll()
+
+
+
+    }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
+
+    const loadTransactionsByEmployee = useCallback(
+    async (employeeId: string) => {
+      paginatedTransactionsUtils.invalidateData()
+      await transactionsByEmployeeUtils.fetchById(employeeId)
+    },
+    [paginatedTransactionsUtils, transactionsByEmployeeUtils]
+  )
+
+  useEffect(() => {
+    if (employees === null && !employeeUtils.loading) {
+        loadAllTransactions()
+    }
+  }, [employeeUtils.loading, employees, loadAllTransactions])
+
+  return (
+    <Fragment>
+      <main className="MainContainer">
+        <Instructions />
+
+        <hr className="RampBreak--l" />
+
+        <InputSelect<Employee>
+          isLoading={isLoading}
+          defaultValue={EMPTY_EMPLOYEE}
+          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          label="Filter by employee"
+          loadingLabel="Loading employees"
+          parseItem={(item) => ({
+            value: item.id,
+            label: `${item.firstName} ${item.lastName}`,
+          })}
+          onChange={async (newValue) => {
+              setAllTransactions(null);
+              if (newValue === null) {
+              return
+            }
+            newValue.id !== ""
+                ? await loadTransactionsByEmployee(newValue.id)
+                : await loadAllTransactions()
+          }}
+          /* Bug 3 */
+        />
+
+        <div className="RampBreak--l" />
+
+        <div className="RampGrid">
+            <Transactions transactions={allTransactions} />
+
+          {transactions !== null && (
+            <button
+              className="RampButton"
+              disabled={
+                paginatedTransactionsUtils.loading || paginatedTransactions?.nextPage == null
+              }
+              //Bug 6 fix part 1
+              onClick={async () => {
+                await loadAllTransactions()
+              }}
+            >
+              View More
+            </button>
+          )}
+        </div>
+      </main>
+    </Fragment>
+  )
+}
---
+++ src/components/Transactions/TransactionPane.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import { InputCheckbox } from "../InputCheckbox"
+import { TransactionPaneComponent } from "./types"
+
+export const TransactionPane: TransactionPaneComponent = ({
+  transaction,
+  loading,
+  setTransactionApproval: consumerSetTransactionApproval,
+}) => {
+  const [approved, setApproved] = useState(transaction.approved)
+
+  return (
+    <div className="RampPane">
+      <div className="RampPane--content">
+        <p className="RampText">{transaction.merchant} </p>
+        <b>{moneyFormatter.format(transaction.amount)}</b>
+        <p className="RampText--hushed RampText--s">
+          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+        </p>
+      </div>
+      <InputCheckbox
+        id={transaction.id}
+        checked={approved}
+        disabled={loading}
+        onChange={async (newValue) => {
+            console.log("Current approved value:", approved);
+            console.log("New value to set:", newValue);
+
+            await consumerSetTransactionApproval({
+            transactionId: transaction.id,
+            newValue,
+          })
+            console.log("After setting approval, approved value:", approved);
+
+            setApproved(newValue);
+            console.log("After calling setApproved, approved value:", approved);
+
+        }}
+      />
+    </div>
+  )
+}
+
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
